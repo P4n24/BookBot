1921a83bfda027d29fd136a815841eb29190964b
Nedas <nedbern4@gmail.com>
2026-01-20 11:39:31 +0200

    G: added character count in stats.py and linked it to main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from stats import get_num_words
+from stats import get_num_words, get_char_count
 
 with open("./books/frankenstein.txt") as f:
 	file_contents = f.read()
 
 print(get_num_words(file_contents))
+print(get_char_count(file_contents))
--- stats.py
+++ stats.py
@@ -1,2 +1,14 @@
 def get_num_words(file_contents):
         return f"Found {len(file_contents.split())} total words"
+
+def get_char_count(file_contents):
+	char_count = {}
+	#loop through the string
+	for i in file_contents.lower():
+		# if the key is not already in the dictionary, add it with value of 1
+		if i not in char_count:
+			char_count[i] = 1
+		# else create add 1 to the value
+		else:
+			char_count[i] += 1
+	return char_count
